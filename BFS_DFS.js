acf08044749945b90a4c0623b42cc7dc9d06b024
rabbledoo <scott.tarnowsky@gmail.com>
2024-10-16 07:19:57 -0400

    saving changes

--- BFS_DFS.js
+++ BFS_DFS.js
@@ -104,6 +104,18 @@ class Tree {
       if (node.left) traverse(node.left);
       if (node.right) traverse(node.right);
     }
+    traverse(current);
+    return data;
+  }
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
   }
 }
 
---
+++ DFS_postorder.js
@@ -0,0 +1,12 @@
+/*
+Create variable to store values of nodes visited
+Store root of the BST in a variable called current
+Write helper function which accepts node
+    If node has left property, call helper function with left property on node
+    If node has right property, call helper function with right property on node
+    Push value of the node to variable that stores the values
+    Invoke helper function with current variable 
+Return array of values
+    
+
+*/
