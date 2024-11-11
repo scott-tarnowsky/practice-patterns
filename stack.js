b6871f6cd6c1f8786c952df6fbf46d64281ef5e8
rabbledoo <scott.tarnowsky@gmail.com>
2024-11-11 11:01:11 -0500

    finished stack

--- stack.js
+++ stack.js
@@ -4,12 +4,52 @@ LIFO -- Last In First Out
 
 */
 
-let stack = [];
+// Array implementation
+// let stack = [];
 
-stack.push("google")
-stack.push("youtube")
-stack.push("instagram")
+// stack.push("google")
+// stack.push("youtube")
+// stack.push("instagram")
 
-console.log(stack.pop())
-console.log(stack.pop())
-console.log(stack.pop())
+// console.log(stack.pop())
+// console.log(stack.pop())
+// console.log(stack.pop())
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      var temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
